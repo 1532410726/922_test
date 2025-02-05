1bf3b5c2051fa7b1838e2a1f53880eb0b435cbf5
922 <1532410726@qq.com>
2025-02-05 18:05:39 +0800

    test_get_account_list接口参数化

--- DJ/tiaoshi.py
+++ DJ/tiaoshi.py
@@ -1,23 +1,64 @@
 import json
-
 import pytest
 import requests
+from L1.log_utils import logger
 
 
-def get_account():
-    url = "https://apitest.dingdingclub.com/sv-invest/kuaishou/baseData/getAccount"
-    token = "eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJmNDg2ZTNjZjc4MWQ0ODIzOGJmNGI2ZjIzNmZkNDA3OSIsInVuYW1lIjoi5p2O5rKb5bOwIiwibmlja05hbWUiOiIiLCJwaG9uZSI6IiIsImVtYWlsIjoiIiwic3ViIjoiMjQifQ.1WB0gZHhGUQMAwnbJl_GIB9S1tBpYx3dEhQh3Ngcu-w1YyVJDbfhcPy1vgwXHnh0Qkt4ZFPYsizG9XwwIEGFqA"
-    headers = {
-        'Authorization': f"{token}",
-        'Content-Type': 'application/json',  # 其他常用头部设置
-        '__auth_app_name__': 'sv-invest'
+@pytest.fixture(scope="module")
+def token():
+    """获取token的fixture，整个模块只执行一次"""
+    url = "https://apitest.dingdingclub.com/sv-invest/login/userLogin"
+    body = {
+        "login": "lipeifeng", "code": "", "password": "123456"
     }
-    r = requests.post(url, headers=headers, json={})
-    # account_data = r.json()
+    r = requests.post(url, json=body)
+    response_data = r.json()
+    token = response_data["data"]["token"]
     assert r.status_code == 200
-    print(r.text)
-    # assert account_data["message"] == "OK!"
+    assert response_data["message"] == "OK!"
+    return token
 
 
-if __name__ == '__main__':
-    get_account()
+class TestShortPlay:
+    def test_get_account(self, token):
+        """获取快手账号"""
+        url = "https://apitest.dingdingclub.com/sv-invest/kuaishou/baseData/getAccount"
+        headers = {
+            'Authorization': f"{token}",
+            'Content-Type': 'application/json',
+            '__auth_app_name__': 'sv-invest'
+        }
+        r = requests.post(url, headers=headers, json={})
+        account_data = r.json()
+        assert r.status_code == 200
+        assert account_data["message"] == "OK!"
+    @pytest.mark.parametrize(
+        "body", [
+            {
+                "pageNo": 1,
+                "pageSize": 10,
+                "selfSetFootPosition": "true",
+                "ivtUserId": "24",
+                "startDate": "2024-12-24",
+                "endDate": "2025-01-22"
+            },
+            {
+                "pageNo": 1,
+                "pageSize": 10,
+                "selfSetFootPosition": "true",
+                "ivtUserId": "24"
+            }
+        ]
+    )
+    def test_get_account_list(self, token, body):
+        """获取快手媒体账户统计列表"""
+        url = "https://apitest.dingdingclub.com/sv-invest/kuaishou/accountList"
+        headers = {
+            'Authorization': f"{token}",
+            'Content-Type': 'application/json',
+            '__auth_app_name__': 'sv-invest'
+        }
+        r = requests.post(url, headers=headers, json=body)
+        account_data = r.json()
+        assert r.status_code == 200
+        assert account_data["message"] == "OK!"
